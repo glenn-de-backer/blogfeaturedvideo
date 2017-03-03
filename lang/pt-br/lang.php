6a3bb0d25efe1b8a5d22e6f2d092a58acd8ca3a2
Ronaldo Ribeiro de Sousa <ribbsousa@gmail.com>
2017-03-03 12:02:20 -0300

    Add language pt-br
    Add language pt-br

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+    return [
+        'backend' => [
+            'featuredvideo' => 'Vídeo em destaque',
+            'description' => 'Conteúdo do iframe de vídeo',
+            'featuredimages' => 'Imagens do vídeo em destaque',
+            'featuredimages_description' => 'Adicionar imagem em destaque ao seu vídeo'
+        ]
+    ];
